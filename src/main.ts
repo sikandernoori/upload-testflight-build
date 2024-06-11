bfb2885ce00f1de2aa59c823ac7b6916c2db48d7
Skandar Munir Ahmed <sikandernoori@gmail.com>
2024-06-11 17:46:04 +0300

    so

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function run(): Promise<void> {
     await altool.installPrivateKey(apiKeyId, apiPrivateKey)
 
     const uploadWithRetry = async (): Promise<void> => {
-      core.debug('Retrying upload...')
+      core.setOutput('Retrying upload...')
       await altool.uploadApp(appPath, appType, apiKeyId, issuerId, options)
       if (output.includes('timeout')) {
         throw new Error('Upload failed due to timeout')
